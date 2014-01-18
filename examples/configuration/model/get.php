07e8235f6521ce272ce179e22f3cbd205a9e283c
Michael Roterman <michael@wtfz.net>
2014-01-19 00:16:14 +0100

    Adding configuration example

---
+++ examples/configuration/model/get.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * This file is part of the Tmdb PHP API created by Michael Roterman.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @package Tmdb
+ * @author Michael Roterman <michael@wtfz.net>
+ * @copyright (c) 2013, Michael Roterman
+ * @version 0.0.1
+ */
+header('Content-Type: text/html; charset=utf-8');
+
+require_once('../../../vendor/autoload.php');
+require_once('../../../apikey.php');
+
+$token  = new \Tmdb\ApiToken(TMDB_API_KEY);
+$client = new \Tmdb\Client($token);
+
+$configRepository = new \Tmdb\Repository\ConfigurationRepository($client);
+$config = $configRepository->load();
+
+var_dump($config);
