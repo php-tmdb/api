d043fae1dff90ab06ddbe4b44560be9ff93e4918
Michael Roterman <michael@wtfz.net>
2014-02-23 01:30:54 +0100

    Adding CertificationRepositoryTest

---
+++ test/Tmdb/Tests/Repository/CertificationRepositoryTest.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * This file is part of the Tmdb PHP API created by Michael Roterman.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @package Tmdb
+ * @author Michael Roterman <michael@wtfz.net>
+ * @copyright (c) 2013, Michael Roterman
+ * @version 0.0.1
+ */
+namespace Tmdb\Tests\Repository;
+
+class CertificationRepositoryTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function shouldLoadCertifications()
+    {
+        $repository = $this->getRepositoryWithMockedHttpClient();
+
+        $repository->getMovieList();
+    }
+
+    protected function getApiClass()
+    {
+        return 'Tmdb\Api\Certification';
+    }
+
+    protected function getRepositoryClass()
+    {
+        return 'Tmdb\Repository\CertificationRepository';
+    }
+}
