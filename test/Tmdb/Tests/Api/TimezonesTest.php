93842bbe6cd5d2004227e568a065a60d3d64ab68
Michael Roterman <michael@wtfz.net>
2014-04-01 23:47:56 +0200

    Adding test

---
+++ test/Tmdb/Tests/Api/TimezonesTest.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * This file is part of the Tmdb PHP API created by Michael Roterman.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @package Tmdb
+ * @author Michael Roterman <michael@wtfz.net>
+ * @copyright (c) 2013, Michael Roterman
+ * @version 0.0.1
+ */
+namespace Tmdb\Tests\Api;
+
+class TimezonesTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function shouldGetTimezones()
+    {
+        $api = $this->getApiMock();
+        $api->expects($this->once())
+            ->method('get')
+            ->with('timezones/list');
+
+        $api->getTimezones();
+    }
+
+    protected function getApiClass()
+    {
+        return 'Tmdb\Api\Timezones';
+    }
+}
