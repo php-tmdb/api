c796003e1d7e88b9578338972d5a9d3f8b6daa2c
Michael Roterman <michael@wtfz.net>
2015-06-21 22:38:39 +0200

    Releasing 2.0.8

--- test/Tmdb/Tests/Repository/TvRepositoryTest.php
+++ test/Tmdb/Tests/Repository/TvRepositoryTest.php
@@ -27,7 +27,7 @@ class TvRepositoryTest extends TestCase
             ->method('get')
             ->with($this->getRequest(
                 'tv/' . self::TV_ID,
-                ['append_to_response' => 'credits,external_ids,images,translations,similar,keywords,changes,content_ratings']
+                ['append_to_response' => 'credits,external_ids,images,translations,similar,keywords,changes,content_ratings,alternative_titles']
             ))
         ;
 
