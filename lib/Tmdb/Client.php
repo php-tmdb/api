dfdbb4e4d716127ede11d6b3d037cf6c5816d56c
Michael Roterman <michael@wtfz.net>
2016-04-06 19:40:00 +0200

    Releasing 2.0.16

--- lib/Tmdb/Client.php
+++ lib/Tmdb/Client.php
@@ -31,7 +31,7 @@ class Client
     use ApiMethodsTrait;
 
     /** Client Version */
-    const VERSION  = '2.0.15';
+    const VERSION  = '2.0.16';
 
     /** Base API URI */
     const TMDB_URI = 'api.themoviedb.org/3/';
