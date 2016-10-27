19bfa635cd97157eb0b6ff6e23ae1e80764337cc
Michael Roterman <michael@wtfz.net>
2016-10-27 22:23:58 +0200

    Prepare 2.1.4

--- lib/Tmdb/Client.php
+++ lib/Tmdb/Client.php
@@ -32,7 +32,7 @@ class Client
     use ApiMethodsTrait;
 
     /** Client Version */
-    const VERSION  = '2.1.0';
+    const VERSION  = '2.1.4';
 
     /** Base API URI */
     const TMDB_URI = 'api.themoviedb.org/3/';
