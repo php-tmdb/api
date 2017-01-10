24742d31840e0008ac91d0ade537eea0c4dc8bd2
Oliver Kaufmann <okaufmann91@gmail.com>
2017-01-10 01:44:34 +0100

    Add properties of tv shows to MovieCredit and name it more generic

--- lib/Tmdb/Factory/PeopleFactory.php
+++ lib/Tmdb/Factory/PeopleFactory.php
@@ -130,7 +130,7 @@ class PeopleFactory extends AbstractFactory
                 if (array_key_exists('cast', $data[$type])) {
                     $cast = $this->createCustomCollection(
                         $data[$type]['cast'],
-                        new Person\MovieCredit(),
+                        new Person\Credit(),
                         new People\Cast()
                     );
 
@@ -144,7 +144,7 @@ class PeopleFactory extends AbstractFactory
                 if (array_key_exists('crew', $data[$type])) {
                     $crew = $this->createCustomCollection(
                         $data[$type]['crew'],
-                        new Person\MovieCredit(),
+                        new Person\Credit(),
                         new People\Crew()
                     );
 
