077b05b9b4d6b3de1ba70e1da1faa4f01087dcd0
Yury Buldakov <yury.buldakov@gmail.com>
2017-06-02 05:54:19 +0300

    Fix version and set correct branch alias

--- lib/Tmdb/Client.php
+++ lib/Tmdb/Client.php
@@ -31,7 +31,7 @@ class Client
     use ApiMethodsTrait;
 
     /** Client Version */
-    const VERSION  = '2.1.7';
+    const VERSION  = '2.1.8';
 
     /** Base API URI */
     const TMDB_URI = 'api.themoviedb.org/3/';
