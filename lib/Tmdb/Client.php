a0a9635aec8c211821b3e364526b3629212dda1e
Neil Daniels <neil.here@gmail.com>
2026-01-30 13:40:33 -0800

    Fixes for dependency updates

--- lib/Tmdb/Client.php
+++ lib/Tmdb/Client.php
@@ -22,8 +22,6 @@ use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\ResponseFactoryInterface;
 use Psr\Http\Message\StreamFactoryInterface;
 use Psr\Http\Message\UriFactoryInterface;
-use Symfony\Component\OptionsResolver\Exception\InvalidOptionsException;
-use Symfony\Component\OptionsResolver\Options;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Tmdb\HttpClient\HttpClient;
 use Tmdb\Token\Api\ApiToken;
@@ -66,11 +64,6 @@ class Client
      */
     private $options = [];
 
-    private static function debugType($value): string
-    {
-        return is_object($value) ? get_class($value) : gettype($value);
-    }
-
     /**
      * Construct our client
      *
@@ -102,25 +95,52 @@ class Client
                 'base_uri' => null,
                 'api_token' => null,
                 'guest_session_token' => null,
-                'http' => [
-                    'client' => null,
-                    'request_factory' => null,
-                    'response_factory' => null,
-                    'stream_factory' => null,
-                    'uri_factory' => null,
-                ],
-                'hydration' => [
-                    'event_listener_handles_hydration' => false,
-                    'only_for_specified_models' => [],
-                ],
-                'event_dispatcher' => [
-                    'adapter' => new class implements EventDispatcherInterface {
-                        public function dispatch(object $event)
-                        {
-                            return $event;
-                        }
-                    },
-                ],
+                'http' => function (OptionsResolver $optionsResolver) {
+                    $optionsResolver->setDefaults(
+                        [
+                            'client' => null,
+                            'request_factory' => null,
+                            'response_factory' => null,
+                            'stream_factory' => null,
+                            'uri_factory' => null,
+                        ]
+                    );
+                    $optionsResolver->setRequired(
+                        [
+                            'client',
+                            'request_factory',
+                            'response_factory',
+                            'stream_factory',
+                            'uri_factory'
+                        ]
+                    );
+                    $optionsResolver->setAllowedTypes('client', [ClientInterface::class, 'null']);
+                    $optionsResolver->setAllowedTypes('request_factory', [RequestFactoryInterface::class, 'null']);
+                    $optionsResolver->setAllowedTypes('response_factory', [ResponseFactoryInterface::class, 'null']);
+                    $optionsResolver->setAllowedTypes('stream_factory', [StreamFactoryInterface::class, 'null']);
+                    $optionsResolver->setAllowedTypes('uri_factory', [UriFactoryInterface::class, 'null']);
+                },
+                'hydration' => function (OptionsResolver $optionsResolver) {
+                    $optionsResolver->setDefaults(
+                        [
+                            'event_listener_handles_hydration' => false,
+                            'only_for_specified_models' => []
+                        ]
+                    );
+                    $optionsResolver->setAllowedTypes('event_listener_handles_hydration', ['bool']);
+                    // @todo 4.1 validate these are actually models
+                    $optionsResolver->setAllowedTypes('only_for_specified_models', ['array']);
+                },
+                'event_dispatcher' => function (OptionsResolver $optionsResolver) {
+                    $optionsResolver->setDefaults(
+                        [
+                            'adapter' => null
+                        ]
+                    );
+
+                    $optionsResolver->setRequired(['adapter']);
+                    $optionsResolver->setAllowedTypes('adapter', [EventDispatcherInterface::class]);
+                }
             ]
         );
 
@@ -141,82 +161,6 @@ class Client
         $resolver->setAllowedTypes('secure', ['bool']);
         $resolver->setAllowedTypes('http', ['array']);
         $resolver->setAllowedTypes('event_dispatcher', ['array']);
-        $resolver->setAllowedTypes('hydration', ['array']);
-
-        $resolver->setNormalizer('http', function (Options $options, $value) {
-            if (!is_array($value)) {
-                throw new InvalidOptionsException(sprintf('The option "http" is expected to be of type "array", but is of type "%s".', self::debugType($value)));
-            }
-
-            $allowedKeys = ['client', 'request_factory', 'response_factory', 'stream_factory', 'uri_factory'];
-            $unknownKeys = array_diff(array_keys($value), $allowedKeys);
-            if (!empty($unknownKeys)) {
-                throw new InvalidOptionsException(sprintf('The option "http" has unknown keys: %s.', implode(', ', $unknownKeys)));
-            }
-
-            foreach ($allowedKeys as $key) {
-                if (!array_key_exists($key, $value)) {
-                    throw new InvalidOptionsException(sprintf('The option "http" must define the key "%s".', $key));
-                }
-            }
-
-            if ($value['client'] !== null && !$value['client'] instanceof ClientInterface) {
-                throw new InvalidOptionsException(sprintf('The option "http[client]" is expected to be an instance of %s or null, but got %s.', ClientInterface::class, self::debugType($value['client'])));
-            }
-            if ($value['request_factory'] !== null && !$value['request_factory'] instanceof RequestFactoryInterface) {
-                throw new InvalidOptionsException(sprintf('The option "http[request_factory]" is expected to be an instance of %s or null, but got %s.', RequestFactoryInterface::class, self::debugType($value['request_factory'])));
-            }
-            if ($value['response_factory'] !== null && !$value['response_factory'] instanceof ResponseFactoryInterface) {
-                throw new InvalidOptionsException(sprintf('The option "http[response_factory]" is expected to be an instance of %s or null, but got %s.', ResponseFactoryInterface::class, self::debugType($value['response_factory'])));
-            }
-            if ($value['stream_factory'] !== null && !$value['stream_factory'] instanceof StreamFactoryInterface) {
-                throw new InvalidOptionsException(sprintf('The option "http[stream_factory]" is expected to be an instance of %s or null, but got %s.', StreamFactoryInterface::class, self::debugType($value['stream_factory'])));
-            }
-            if ($value['uri_factory'] !== null && !$value['uri_factory'] instanceof UriFactoryInterface) {
-                throw new InvalidOptionsException(sprintf('The option "http[uri_factory]" is expected to be an instance of %s or null, but got %s.', UriFactoryInterface::class, self::debugType($value['uri_factory'])));
-            }
-
-            return $value;
-        });
-
-        $resolver->setNormalizer('hydration', function (Options $options, $value) {
-            if (!is_array($value)) {
-                throw new InvalidOptionsException(sprintf('The option "hydration" is expected to be of type "array", but is of type "%s".', self::debugType($value)));
-            }
-
-            $allowedKeys = ['event_listener_handles_hydration', 'only_for_specified_models'];
-            $unknownKeys = array_diff(array_keys($value), $allowedKeys);
-            if (!empty($unknownKeys)) {
-                throw new InvalidOptionsException(sprintf('The option "hydration" has unknown keys: %s.', implode(', ', $unknownKeys)));
-            }
-
-            if (!array_key_exists('event_listener_handles_hydration', $value) || !is_bool($value['event_listener_handles_hydration'])) {
-                throw new InvalidOptionsException('The option "hydration[event_listener_handles_hydration]" is expected to be of type "bool".');
-            }
-            if (!array_key_exists('only_for_specified_models', $value) || !is_array($value['only_for_specified_models'])) {
-                throw new InvalidOptionsException('The option "hydration[only_for_specified_models]" is expected to be of type "array".');
-            }
-
-            return $value;
-        });
-
-        $resolver->setNormalizer('event_dispatcher', function (Options $options, $value) {
-            if (!is_array($value)) {
-                throw new InvalidOptionsException(sprintf('The option "event_dispatcher" is expected to be of type "array", but is of type "%s".', self::debugType($value)));
-            }
-
-            $allowedKeys = ['adapter'];
-            $unknownKeys = array_diff(array_keys($value), $allowedKeys);
-            if (!empty($unknownKeys)) {
-                throw new InvalidOptionsException(sprintf('The option "event_dispatcher" has unknown keys: %s.', implode(', ', $unknownKeys)));
-            }
-
-            if (!array_key_exists('adapter', $value) || !$value['adapter'] instanceof EventDispatcherInterface) {
-                throw new InvalidOptionsException(sprintf('The option "event_dispatcher[adapter]" is expected to be an instance of %s.', EventDispatcherInterface::class));
-            }
-
-            return $value;
-        });
 
         // @todo 4.1 fix smelly stuff
         $resolver->setAllowedTypes(
